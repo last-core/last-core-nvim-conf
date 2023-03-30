bbee1c569a3f470494e91bcdbd7b7f4b49166413
l.castro <lorenzo.castro901@gmail.com>
2023-03-30 15:25:42 +0200

    changed settings

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -10,7 +10,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true 
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -18,6 +18,8 @@ vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.termguicolors = true
 
